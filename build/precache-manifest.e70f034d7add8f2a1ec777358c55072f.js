0e0d280d3ef47aa5ff42f5242ca64eee7124c220
Ram <rammurthy.kota@gmail.com>
2020-09-08 22:22:03 +0530

    add build

---
+++ build/precache-manifest.e70f034d7add8f2a1ec777358c55072f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e5eeff26b5959eebbbab481d436bba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e7146d2df07228301de",
+    "url": "/static/css/main.d45acd62.chunk.css"
+  },
+  {
+    "revision": "cdc901eb5c71b4dac8c1",
+    "url": "/static/js/2.b678a4f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b678a4f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e7146d2df07228301de",
+    "url": "/static/js/main.218a570a.chunk.js"
+  },
+  {
+    "revision": "558492f713724e610d91",
+    "url": "/static/js/runtime-main.d4637d08.js"
+  }
+]);
